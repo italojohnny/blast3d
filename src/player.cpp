7cbf3530a2ea408ffc5f64e49a40dbb1e7e5a3e1
Italo Johnny <italo@forip.com.br>
2015-11-19 18:03:03 -0200

    new design the aircraft

--- src/player.cpp
+++ src/player.cpp
@@ -22,7 +22,7 @@ void Player::draws (void)
 	glEnd ();
 
 	glColor3f (0.15f, 0.15f,0.15f);
-	glColor3f (1, 0, 0);
+	//glColor3f (1, 0, 0);
 	glBegin(GL_TRIANGLES);
 		glVertex3f(5.0f, 0.0f,-01.0f);glVertex3f(3.0f, 0.0f,-05.0f);glVertex3f(7.0f, 0.0f,-05.0f);
 		glVertex3f(5.0f, 7.5f,-15.0f);glVertex3f(3.0f, 0.0f,-05.0f);glVertex3f(7.0f, 0.0f,-05.0f);
@@ -36,36 +36,54 @@ void Player::draws (void)
 		glVertex3f(5.0f, 0.0f,-01.0f);glVertex3f(5.0f,-7.5f,-15.0f);glVertex3f(7.0f, 0.0f,-05.0f);
 		glVertex3f(5.0f, 0.0f,-01.0f);glVertex3f(3.0f, 0.0f,-05.0f);glVertex3f(5.0f,-7.5f,-15.0f);
 	glEnd ();
-/*
+	glBegin(GL_TRIANGLES);
+		glVertex3f(15.0f, 0.0f,-17.0f);glVertex3f(7.0f, 0.0f,-2.0f);glVertex3f(7.0f, 1.0f,-8.0f);
+		glVertex3f(15.0f, 0.0f,-17.0f);glVertex3f(7.0f, 0.0f,-2.0f);glVertex3f(7.0f,-1.0f,-8.0f);
+		glVertex3f(15.0f, 0.0f,-17.0f);glVertex3f(7.0f, 1.0f,-8.0f);glVertex3f(7.0f,-1.0f,-8.0f);
+		glVertex3f( 7.0f, 0.0f,-02.0f);glVertex3f(7.0f, 1.0f,-8.0f);glVertex3f(7.0f,-1.0f,-8.0f);
+	glEnd();
+
 	glColor3f (0.15f, 0.15f,0.15f);
-	//glColor3f (0, 0, 1);
+	//glColor3f (1, 0, 0);
 	glBegin(GL_TRIANGLES);
 		glVertex3f(-5.0f, 0.0f,-01.0f);glVertex3f(-3.0f, 0.0f,-05.0f);glVertex3f(-7.0f, 0.0f,-05.0f);
-		glVertex3f(-5.0f, 7.5f,-15.0f);glVertex3f(-3.0f, 0.0f,-04.0f);glVertex3f(-7.0f, 0.0f,-05.0f);
+		glVertex3f(-5.0f, 7.5f,-15.0f);glVertex3f(-3.0f, 0.0f,-05.0f);glVertex3f(-7.0f, 0.0f,-05.0f);
 		glVertex3f(-5.0f, 0.0f,-01.0f);glVertex3f(-5.0f, 7.5f,-15.0f);glVertex3f(-7.0f, 0.0f,-05.0f);
 		glVertex3f(-5.0f, 0.0f,-01.0f);glVertex3f(-3.0f, 0.0f,-05.0f);glVertex3f(-5.0f, 7.5f,-15.0f);
 	glEnd ();
-	//glColor3f (1, 1, 0);
+	//glColor3f (0, 1, 0);
 	glBegin(GL_TRIANGLES);
 		glVertex3f(-5.0f, 0.0f,-01.0f);glVertex3f(-3.0f, 0.0f,-05.0f);glVertex3f(-7.0f, 0.0f,-05.0f);
 		glVertex3f(-5.0f,-7.5f,-15.0f);glVertex3f(-3.0f, 0.0f,-05.0f);glVertex3f(-7.0f, 0.0f,-05.0f);
 		glVertex3f(-5.0f, 0.0f,-01.0f);glVertex3f(-5.0f,-7.5f,-15.0f);glVertex3f(-7.0f, 0.0f,-05.0f);
 		glVertex3f(-5.0f, 0.0f,-01.0f);glVertex3f(-3.0f, 0.0f,-05.0f);glVertex3f(-5.0f,-7.5f,-15.0f);
 	glEnd ();
-*/	
-glColor3f(0, 0, 1);	
+	glBegin(GL_TRIANGLES);
+		glVertex3f(-15.0f, 0.0f,-17.0f);glVertex3f(-7.0f, 0.0f,-2.0f);glVertex3f(-7.0f, 1.0f,-8.0f);
+		glVertex3f(-15.0f, 0.0f,-17.0f);glVertex3f(-7.0f, 0.0f,-2.0f);glVertex3f(-7.0f,-1.0f,-8.0f);
+		glVertex3f(-15.0f, 0.0f,-17.0f);glVertex3f(-7.0f, 1.0f,-8.0f);glVertex3f(-7.0f,-1.0f,-8.0f);
+		glVertex3f( -7.0f, 0.0f,-02.0f);glVertex3f(-7.0f, 1.0f,-8.0f);glVertex3f(-7.0f,-1.0f,-8.0f);
+	glEnd();
+
+
+/*
+
+	glColor3f(0.25, 0.5, 0.25);	
 	glBegin(GL_TRIANGLES);
 		glVertex3f( 15.0f, 0.0f,-20.0f);glVertex3f( 5.0f, 0.0f, -1.0f);glVertex3f( 5.0f, 2.2f, -5.0f);
-		glVertex3f( 15.0f, 0.0f,-20.0f);glVertex3f( 5.0f, 0.0f, -1.0f);glVertex3f( 5.0f,-2.2f, -5.0f);
+		//glVertex3f( 15.0f, 0.0f,-20.0f);glVertex3f( 5.0f, 0.0f, -1.0f);glVertex3f( 5.0f,-2.2f, -5.0f);
+glColor3f(1,0,0);
+		glVertex3f( 15.0f, 0.0f,-20.0f);glVertex3f( 6.7, 1.8,-7.5);glVertex3f( 6.7, 0, -7.5);
 	glEnd();
-	
 	glColor3f(1,1,1);
+	
 	glBegin(GL_LINES);
-
 		glVertex3f(15.0f, 0.0f,-20.0f);glVertex3f( 5.0f, 0.0f, -1.0f);
 		glVertex3f(15.0f, 0.0f,-20.0f);glVertex3f( 5.0f, 2.2f, -5.0f);
-
 		glVertex3f( 5.0f, 0.0f, -1.0f);glVertex3f(5, 7.5f , -15.0f);
 	glEnd();
+*/
+
+
 
 }
